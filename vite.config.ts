19f9936d68a9131681d47992707897db356651b5
unknown <sashakotik450697@gmail.com>
2024-08-08 17:35:57 +0300

    Import libraries

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  resolve: {
+      alias: {
+          assets: "/src/assets",
+          components: "/src/components",
+          pages: "/src/pages",
+          interfaces: "/src/interfaces",
+      },
+  },
+  preview: {
+      port: 5173,
+      strictPort: true,
+  },
+  server: {
+      port: 5173,
+      strictPort: true,
+      host: true,
+      origin: "http://0.0.0.0:5173",
+  },
+});
