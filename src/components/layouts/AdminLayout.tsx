42bc31aebf5d0c2f9ed0ae76bd2e352c8a8aa74c
unknown <sashakotik450697@gmail.com>
2024-09-08 15:21:18 +0300

    update app.tsx

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import Header from "../partials/Header.tsx";
 import Sidebar from "../partials/Sidebar.tsx";
 
+import MusicPlayer from "components/MusicPlayer.tsx";
+
 const AdminLayout = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -15,6 +17,7 @@ const AdminLayout = () => {
                 <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
                 <main className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto scrollbar scrollbar-thumb-sky-700 scrollbar-track-sky-300 overflow-y-scroll">
                     <Outlet />
+                    <MusicPlayer />
                 </main>
             </div>
         </div>
