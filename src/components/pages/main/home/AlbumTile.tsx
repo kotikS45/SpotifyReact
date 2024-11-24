0778bde6a04d9324c10404c39ed7b13fff631260
Kuchulap Nazar <kuculapnazar@gmail.com>
2024-11-24 17:13:49 +0200

    fix albums page

--- src/components/pages/main/home/AlbumTile.tsx
+++ src/components/pages/main/home/AlbumTile.tsx
@@ -5,9 +5,11 @@ import { API_URL } from "utils/envData";
 
 interface IAlbumTileProps {
   album: IAlbum
+  width?: string;
+  height?: string;
 }
 
-const AlbumTile: React.FC<IAlbumTileProps> = ({ album }) => {
+const AlbumTile: React.FC<IAlbumTileProps> = ({ album, width = "145px", height = "145px" }) => {
   const navigate = useNavigate();
   const imageSrc = API_URL + "/Images/800_";
 
@@ -16,8 +18,8 @@ const AlbumTile: React.FC<IAlbumTileProps> = ({ album }) => {
   };
 
   return (
-    <div className="flex flex-col w-[145px] cursor-pointer" onClick={handleClick}>
-      <img src={imageSrc.concat(album.image)} alt={album.name} className="w-[145px] h-[145px] object-cover mx-auto rounded-[16px]"/>
+    <div className="flex flex-col cursor-pointer" style={{ width }} onClick={handleClick}>
+      <img src={imageSrc.concat(album.image)} alt={album.name} className="object-cover mx-auto rounded-[16px]" style={{ width, height }}/>
       <span className="font-roboto font-medium text-white text-xl pt-[10px] truncate">{album.name}</span>
       <span className="font-roboto font-medium text-[#BCBCBC] text-base pt-[4px] truncate">{album.artist.name}</span>
     </div>
